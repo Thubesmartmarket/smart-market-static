09c011fc00b8e5f6131f2c0cfe02a66afc69ef6b
Thubesmartmarket <khambulethubelihle3@gmail.com>
2026-02-12 09:09:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-fetch('data/niches.json')
-  .then(res => res.json())
-  .then(data => {
-    const list = document.getElementById('niche-list');
-    if (list) {
-      data.forEach(n => {
-        const a = document.createElement('a');
-        a.href = `niche.html?n=${encodeURIComponent(n.slug)}`;
-        a.innerText = n.name;
-        list.appendChild(a);
-        list.appendChild(document.createElement('br'));
-      });
-    }
+fetch("data/niches.json")
+.then(res=>res.json())
+.then(niches=>{
+const container=document.getElementById("top-niches");
+niches.forEach(n=>{
+const card=document.createElement("div");
+card.className="card";
 
-    const params = new URLSearchParams(window.location.search);
-    const slug = params.get('n');
-    const niche = data.find(x => x.slug === slug);
-    if (niche) {
-      document.getElementById('niche').innerHTML = `
-        <h1>${niche.name}</h1>
-        <p>${niche.description}</p>
-      `;
-      document.getElementById('offer').href = niche.offer;
-    }
-  });
+card.innerHTML=`
+<h3>${n.emoji} ${n.name}</h3>
+<p>${n.description}</p>
+<a href="niche.html?niche=${n.slug}">Explore</a>
+`;
+container.appendChild(card);
+});
+});
